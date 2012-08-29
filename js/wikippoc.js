30bb71cf0a683c6f4381ec5ff90a062ae490f293
Ed Summers <ehs@pobox.com>
2012-08-29 11:14:46 +0000

    minor cosmetic changes

--- js/wikippoc.js
+++ js/wikippoc.js
@@ -1,15 +1,19 @@
 function wikippoc(url, callback) {
 
   function clean(s) {
-      return s.replace(/ ?[.;:]$/, '');
+    return s.replace(/ ?[.;:]$/, '');
   }
 
+  // fetch json view for the ppoc item
   var jsonUrl = url + "?fo=json";
   $.ajax({url: jsonUrl, dataType: "jsonp", success: function(data) {
     var item = data.item;
     var creators = item.creators.join(", ") || "";
     var repository = item.repository || "Library of Congress Prints and Photographs Division";
+    
+    // generate wikitext from the ppoc metadata
     item.wikitext = '<includeonly>{{#if:{{{creator|}}}|{{{' + creators + '}}}, |}}"{{#if:{{{file|}}}|[[commons:File:{{{file}}}|{{{title}}}]]|{{{' + item.title + '}}}}}". {{{' + clean(item.medium_brief) + '}}}, {{{' + clean(item.date) + '}}}. {{{' + repository + '}}}, [{{{' + item.link + '}}} {{{' + item.reproduction_number + '}}}].</includeonly>';
+
     callback(item);
   }});
 }
